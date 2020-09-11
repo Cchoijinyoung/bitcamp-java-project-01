54de80aac5c1cfa9a6b4b47d6dd950188c0eeefd
unknown <cchoijjinyoung>
2020-09-11 18:14:11 +0900

    first commit

---
+++ src/main/java/com/eomcs/pms/handler/HelloCommand.java
@@ -0,0 +1,19 @@
+package com.eomcs.pms.handler;
+
+
+import java.sql.Date;
+import java.util.List;
+
+import com.eomcs.pms.domain.Board;
+import com.eomcs.util.Prompt;
+
+public class HelloCommand implements Command {
+	
+	@Override
+	public void execute() {
+		System.out.println("안녕하세요!");
+		//TODO Auto-generated method stub
+		
+	}
+
+}
